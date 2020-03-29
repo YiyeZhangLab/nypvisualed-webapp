1ad25c4155d4bd0173fecd49f8f02940f7ca87a1
YiyeZhang <ichiyoz@gmail.com>
2020-03-29 18:18:46 -0400

    test the iframe in about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="about">
     <h1>About here</h1>
-    <iframe
-      src="http://localhost:5601/app/kibana#/dashboard/edf84fe0-e1a0-11e7-b6d5-4dc382ef7f5b?embed=true&_g=(refreshInterval%3A(pause%3A!f%2Cvalue%3A900000)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"
-      height="600"
-      width="800"
-    ></iframe>
+  <iframe src="http://yiyeserver.duckdns.org/k/app/kibana#/dashboard/fa8adaa0-71d8-11ea-b285-8bb2fd0522c9?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3A'2020-03-01T17%3A50%3A00.000Z'%2Cto%3A'2020-03-29T17%3A00%3A00.000Z'))" height="600" width="800"></iframe>
   </div>
 </template>
 

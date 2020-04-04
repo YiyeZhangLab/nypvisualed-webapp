69b2aaf665ab09b32d981eeccc3c293f203f7268
YiyeZhang <ichiyoz@gmail.com>
2020-04-04 19:02:20 -0400

    this change makes the iFrame fill 100% of the width

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="home">
-    <h1>VISUAL(ED)</h1>
-    <iframe
-      id="frameToPrint"
-      src="http://yiyeserver.duckdns.org/k/app/kibana#/dashboard/fa8adaa0-71d8-11ea-b285-8bb2fd0522c9?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3A'2020-03-01T17%3A50%3A00.000Z'%2Cto%3Anow))"
-      height="3300"
-      width="1200"
-    ></iframe>
-    <button v-on:click="printPDF">print</button>
+  <div>
+    <b-row>
+      <b-col>
+        <h1>VISUAL(ED)</h1>
+      </b-col>
+      <b-col>
+        <div class="float-right">
+          Daily Trends Since March 1st, 2020
+          <b-button variant="outline-primary" v-on:click="printPDF">Print Report</b-button>
+        </div>
+      </b-col>
+    </b-row>
+    <b-row class="border border-primary">
+      <b-col class="iframe-container">
+        <iframe
+          id="frameToPrint"
+          src="http://yiyeserver.duckdns.org/k/app/kibana#/dashboard/fa8adaa0-71d8-11ea-b285-8bb2fd0522c9?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3A'2020-03-01T17%3A50%3A00.000Z'%2Cto%3Anow))"
+          height="3000"
+          allowfullscreen
+        ></iframe>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -15,13 +28,34 @@
 export default {
   name: 'Home',
   components: {},
+  created() {
+    setInterval(function() {
+      document.getElementById('frameToPrint').style.height =
+        document.getElementById('frameToPrint').contentWindow.document.documentElement
+          .scrollHeight + 'px';
+    }, 8000);
+  },
   methods: {
     printPDF() {
       var frame = document.getElementById('frameToPrint');
       var framedoc = frame.contentWindow;
       framedoc.print();
-      console.log('blabla');
     }
   }
 };
 </script>
+<style scoped>
+.iframe-container {
+  padding-top: 56.25%;
+  position: relative;
+}
+
+.iframe-container iframe {
+  border: 0;
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+}
+</style>
